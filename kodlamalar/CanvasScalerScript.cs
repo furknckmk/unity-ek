0a9f4bc1ce311ac142337fce1eb914b0b8b6df69
Furkan Çakmak <baruta9898@gmail.com>
2018-12-16 20:04:30 +0300

    Initial commit

---
+++ kodlamalar/CanvasScalerScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class CanvasScalerScript : MonoBehaviour {
+
+	void Awake () {
+		ResizeCanvas ();
+	}
+
+	void Update () {
+		ResizeCanvas ();
+	}
+
+	private void ResizeCanvas () {
+		if(Screen.height > 2560) {
+			gameObject.GetComponent<CanvasScaler>().scaleFactor = (float)((float) Screen.height / 2560f);
+		} else if(Screen.height == 2560){
+			gameObject.GetComponent<CanvasScaler>().scaleFactor = 1;
+		} else if(Screen.height < 2560){
+			gameObject.GetComponent<CanvasScaler>().scaleFactor = (float)(1 / (2560f / (float) Screen.height));
+		}
+	}
+}
